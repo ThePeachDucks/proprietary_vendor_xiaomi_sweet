4c3a7721379afe212a8b608e312da0f07f26dad3
sweetyicecare <aditya.kevin@hotmail.com>
2023-02-06 23:56:33 +0000

    sweet: Build QCOM FM Radio supports
    Signed-off-by: sweetyicecare <aditya.kevin@hotmail.com>

--- sweet-vendor.mk
+++ sweet-vendor.mk
@@ -1136,6 +1136,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libthermalclient \
+    fm_helium \
+    libfm-hci \
+    vendor.qti.hardware.fm@1.0_system_ext \
     CneApp \
     ConnectionSecurityService \
     IWlanService \
